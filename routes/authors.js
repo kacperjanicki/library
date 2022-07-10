217205233c411f05ccfa4c71e0114322d4d3cfb3
kacperjanicki <kacperekjanicki9@gmail.com>
2022-07-10 03:51:33 +0200

    routes

---
+++ routes/authors.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const Author = require("../models/author");
+
+//All authors
+router.get("/", async (req, res) => {
+    let searchOptions = {};
+    if (req.query.name != null && req.query.name !== "") {
+        searchOptions.name = new RegExp(req.query.name, "i");
+    }
+    try {
+        const authors = await Author.find(searchOptions);
+        res.render("authors/index", { authors: authors, searchOptions: req.query });
+    } catch {
+        res.redirect("/");
+    }
+
+    res.render("authors/index");
+});
+
+//New author add
+router.get("/add", (req, res) => {
+    res.render("authors/new", { author: new Author() });
+});
+
+//Create author route
+router.post("/", async (req, res) => {
+    const author = new Author({
+        name: req.body.name,
+    });
+
+    try {
+        const newAuthor = await author.save();
+        // res.redirect(`authors/${newAuthor.name}`)
+        res.redirect("authors");
+    } catch {
+        res.render("authors/new", {
+            author: author,
+            errorMessage: "Error creating author",
+        });
+    }
+
+    // author.save((err, newAuthor) => {
+    //     if (err) {
+    //         res.render("authors/new", {
+    //             author: author,
+    //             errorMessage: "Error creating author",
+    //         });
+    //     } else {
+    //         // res.redirect(`authors/${newAuthor.name}`)
+    //         res.redirect("authors");
+    //     }
+    // });
+});
+
+module.exports = router;
