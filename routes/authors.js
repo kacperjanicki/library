73eb32693a96858782a977e966c812c172dfab3f
kacperjanicki <kacperekjanicki9@gmail.com>
2022-07-11 16:58:53 +0200

    author routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Author = require("../models/author");
+const Book = require("../models/book");
 
 //All authors
 router.get("/", async (req, res) => {
@@ -22,6 +23,54 @@ router.get("/", async (req, res) => {
 router.get("/add", (req, res) => {
     res.render("authors/new", { author: new Author() });
 });
+//Specific author
+router.get("/:id", async (req, res) => {
+    let author = await Author.findById(req.params.id);
+    let booksByAuthor = await Book.find({ author: author });
+
+    res.render("authors/show", { author: author, booksByAuthor: booksByAuthor });
+});
+//Specific author edit
+router.get("/:id/edit", async (req, res) => {
+    const author = await Author.findById(req.params.id);
+    try {
+        res.render("authors/edit", { author: author });
+    } catch {
+        res.redirect("/authors");
+    }
+});
+router.put("/:id", async (req, res) => {
+    let author;
+    try {
+        author = await Author.findById(req.params.id);
+        author.name = req.body.name;
+        await author.save();
+        res.redirect(`/authors/${author.id}`);
+    } catch {
+        if (author == null) {
+            res.redirect("/");
+        } else {
+            res.render("/authors/edit", {
+                author: author,
+                errorMessage: "Error updating author",
+            });
+        }
+    }
+});
+router.delete("/:id", async (req, res) => {
+    let author;
+    try {
+        author = await Author.findById(req.params.id);
+        await author.remove();
+        res.redirect("/authors");
+    } catch {
+        if (author == null) {
+            res.redirect("/");
+        } else {
+            res.redirect(`/authors/${author.id}`);
+        }
+    }
+});
 
 //Create author route
 router.post("/", async (req, res) => {
@@ -31,7 +80,7 @@ router.post("/", async (req, res) => {
 
     try {
         const newAuthor = await author.save();
-        // res.redirect(`authors/${newAuthor.name}`)
+        res.redirect(`authors/${newAuthor.id}`);
         res.redirect("authors");
     } catch {
         res.render("authors/new", {
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
 const bookRouter = require("./routes/books");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -15,6 +16,7 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use(methodOverride("_method"));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.databaseURL, { useNewUrlParser: true });
